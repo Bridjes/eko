9fa8a2d32d2c7bc2266489a902b88379ca870848
Bridjes <iluha.murzhin@gmail.com>
2023-10-03 08:31:18 +0300

    Commit for Stasson with config

---
+++ eko/config.py
@@ -0,0 +1,7 @@
+CFG_SECRET_KEY = 'django-insecure-(6xrk)4hzuzvw3@^^x_jv*_88a(qf@@0+(y)0!k6%)ot_#2t+n'
+
+# DATABSE
+CFG_USER = 'postgres'
+CFG_PASSWORD = '1234'
+CFG_HOST = '192.168.20.156'
+CFG_PORT = '5432'
